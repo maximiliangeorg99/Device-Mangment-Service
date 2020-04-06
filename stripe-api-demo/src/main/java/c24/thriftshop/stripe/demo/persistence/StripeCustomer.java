59376576fed1703938f3e733563c78084f7018b4
Maximilian Georg <maximilian.georg@chrono24.com>
2020-04-06 11:23:02 +0200

    Added StripeCustomer

--- stripe-api-demo/src/main/java/c24/thriftshop/stripe/demo/persistence/StripeCustomer.java
+++ stripe-api-demo/src/main/java/c24/thriftshop/stripe/demo/persistence/StripeCustomer.java
@@ -1,4 +1,27 @@
 package c24.thriftshop.stripe.demo.persistence;
 
 public class StripeCustomer {
+    private String id;
+    private String description;
+    private String email;
+    private String name;
+    private String phone;
+    private String object;
+    private int balance;
+    private String currency;
+
+    public StripeCustomer() {
+
+    }
+
+    public StripeCustomer(String id, String description, String email, String name, String phone, String object, int balance, String currency) {
+        this.id = id;
+        this.description = description;
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.object = object;
+        this.balance = balance;
+        this.currency = currency;
+    }
 }
